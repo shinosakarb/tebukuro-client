af45e28ac7b1fa6acc4bfccfb7d983f0ce200b63
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-15 23:59:22 +0900

    Add Event component to container

--- containers/Event/__test__/EventContainer.spec.js
+++ containers/Event/__test__/EventContainer.spec.js
@@ -2,10 +2,13 @@ import React              from 'react'
 import { mount, shallow } from 'enzyme'
 import ToJson             from 'enzyme-to-json'
 import { Event }          from '../index.js'
+import Params             from '../../../factories/Event'
+
+jest.mock('../../../components/Event', () => 'EventComponent')
 
 describe('Event container', () => {
   it('should render self and subcomponents', () => {
-    const wrapper = shallow(<Event/>)
+    const wrapper = shallow(<Event event={ Params.event1 }/>)
     const tree = ToJson(wrapper)
     expect(tree).toMatchSnapshot()
   })
--- containers/Event/index.js
+++ containers/Event/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { connect }          from 'react-redux'
+import EventComponent       from '../../components/Event'
 
 export class Event extends Component {
   render() {
     return (
-      <div>This is the Event container.</div>
-     )
+      <div>
+        This is the Event container.
+        <EventComponent event={ this.props.event } />
+      </div>
+    )
   }
 }
 
