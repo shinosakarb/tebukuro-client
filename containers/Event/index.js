2f32230043668ef7b059fe3c33cf3a436bef4eaf
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-15 13:47:07 +0900

    Remove action and component from Event container

--- containers/Event/index.js
+++ containers/Event/index.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react'
 import { connect }          from 'react-redux'
-import EventComponent       from '../../components/Event'
-import { showEvent }        from '../../actions/Event'
 
 export class Event extends Component {
-  componentDidMount() {
-    this.props.showEvent(this.props.eventId)
-  }
-
   render() {
     return (
-      <EventComponent event={ this.props.event } />
+      <div>This is the Event container.</div>
      )
   }
 }
 
-const mapStateToProps = (state) => (
-    { event: state.Event.event }
-)
-
-export default connect(mapStateToProps, { showEvent })(Event)
+export default connect(null)(Event)
