167ed0b1016c4d2bdc914851f2d32dd61abdb874
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-16 13:02:51 +0900

    [WIP] Fix code with ESLint

--- containers/Event/__test__/EventContainer.spec.js
+++ containers/Event/__test__/EventContainer.spec.js
@@ -1,14 +1,14 @@
-import React              from 'react'
-import { mount, shallow } from 'enzyme'
-import ToJson             from 'enzyme-to-json'
-import { Event }          from '../index.js'
-import Params             from '../../../factories/Event'
+import React from 'react'
+import { shallow } from 'enzyme'
+import ToJson from 'enzyme-to-json'
+import { Event } from '../index'
+import Params from '../../../factories/Event'
 
 jest.mock('../../../components/Event', () => 'EventComponent')
 
 describe('Event container', () => {
   it('should render self and subcomponents', () => {
-    const wrapper = shallow(<Event event={ Params.event1 }/>)
+    const wrapper = shallow(<Event event={Params.event1} />)
     const tree = ToJson(wrapper)
     expect(tree).toMatchSnapshot()
   })
