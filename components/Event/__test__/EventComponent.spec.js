eaecb01e09ef081252b119dd6d8486e353fd8b2f
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-16 13:54:06 +0900

    Fix component code with ESLint

--- components/Event/__test__/EventComponent.spec.js
+++ components/Event/__test__/EventComponent.spec.js
@@ -1,13 +1,11 @@
-import { shallow }   from 'enzyme'
+import { shallow } from 'enzyme'
 import shallowToJson from 'enzyme-to-json'
-import Event         from '../index.js'
-import Params        from '../../../factories/Event'
+import Event from '../index'
+import Params from '../../../factories/Event'
 
 describe('Event Component', () => {
   it('renders self.', () => {
-    const component = shallow(
-      <Event event={ Params.event1 } />
-    )
+    const component = shallow(<Event event={Params.event1} />)
     const tree = shallowToJson(component)
 
     expect(tree).toMatchSnapshot()
--- components/Event/index.js
+++ components/Event/index.js
@@ -1,8 +1,12 @@
-const Event = (props) => (
+import PropTypes from 'prop-types'
+
+const Event = props => (
   <div>
-    <p>name: { props.event.name }</p>
-    <p>description: { props.event.description }</p>
+    <p>name: {props.event.name}</p>
+    <p>description: {props.event.description}</p>
   </div>
 )
+
+Event.propTypes = { event: PropTypes.instanceOf(Object).isRequired }
 
 export default Event
