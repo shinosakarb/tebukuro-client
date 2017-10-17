189c749d9cc59c07b97cd915fd3ce083d2c93c3c
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-17 20:41:10 +0900

    Add Event type to Event Container

--- containers/Event/index.js
+++ containers/Event/index.js
@@ -1,15 +1,13 @@
+// @flow
 import React from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 import EventComponent from '../../components/Event'
 
-export const Event = props => (
+export const Event = (props: {event: EventProps}) => (
   <div>
     This is the Event container.
     <EventComponent event={props.event} />
   </div>
 )
-
-Event.propTypes = { event: PropTypes.instanceOf(Object).isRequired }
 
 export default connect(null)(Event)
