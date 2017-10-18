1033283f31112a1dc124f0bb5b8fb06f9397fda6
YTorii <metalwarrior_type_jp@hotmail.com>
2017-10-18 18:20:56 +0900

    Fix ESLint errors for importing enzyme in Jest setup file (#18)
    * Add comment to jest setup.js to disable no-extraneous-dependencies
    
    * Use eslint-disable to clarify the range of disabling eslint

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+/* eslint-disable import/no-extraneous-dependencies */
 
 configure({ adapter: new Adapter() })
