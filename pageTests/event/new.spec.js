95f70a61cf6c17776c1f599c5120b08d2dc06d7b
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-21 10:18:34 +0900

    Add Event Contatiner to New Event page

--- pageTests/event/new.spec.js
+++ pageTests/event/new.spec.js
@@ -2,6 +2,8 @@ import { shallow } from 'enzyme'
 import shallowToJson from 'enzyme-to-json'
 import EventFormPage from '../../pages/event/new'
 
+jest.mock('../../containers/EventForm', () => 'Event Container')
+
 describe('Event page', () => {
   it('renders the event page.', () => {
     const component = shallow(<EventFormPage />)
