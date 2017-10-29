67de02e717e708d08bd66100167d37b67083d75d
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-29 16:14:51 +0900

    Remove EventForm Container from New Event Page to avoid netlify build failure

--- pages/event/new.js
+++ pages/event/new.js
@@ -1,10 +1,8 @@
 // @flow
 import React from 'react'
-import EventFormContainer from '../../containers/EventForm'
 
 export default () => (
   <div>
     <h3>This is the event form page!</h3>
-    <EventFormContainer />
   </div>
 )
