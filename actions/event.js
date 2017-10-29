0c714a4c0c9b36d8e3cc912d515d5205e251d238
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-30 08:48:27 +0900

    Add fetch event action and spec file

--- actions/event.js
+++ actions/event.js
@@ -7,14 +7,19 @@ import ActionsType from '../constants/Actions'
 /* eslint-disable */
 // flow-disable-nextline
 import EventAPI from '../api/Event'
-import type { EventProps } from '../types/Event'
+import type { EventId, EventProps } from '../types/Event'
 
 
 const create = createAction(ActionsType.Event.createEvent, EventAPI.create)
+const fetch = createAction(ActionsType.Event.fetchEvent, EventAPI.find)
 
 export const createEvent = (params: EventProps) => (dispatch: Dispatch, getState: Function) => {
     return dispatch(create(params)).then(json => {
        const id = getState().event.id
        Router.replace(`/event/${id}`)
     })
+}
+
+export const fetchEvent = (params: EventId) => (dispatch: Dispatch) => {
+    return dispatch(fetch(params)).catch(err => { })
 }
