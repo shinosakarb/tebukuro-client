2a55f2f56344a0f31e449de824e3b16a492fbb51
ytorii <metalwarrior_type_jp@hotmail.com>
2017-10-30 11:55:25 +0900

    Remove Event Container from Event Page to avoid netlofy build failure

--- pages/event/show.js
+++ pages/event/show.js
@@ -1,11 +1,8 @@
 // @flow
 import React from 'react'
-import EventContainer from '../../containers/Event'
-import type { EventId } from '../../types/Event'
 
-export default (props: {url: {query: EventId}}) => (
+export default () => (
   <div>
     <h3>This is the event page!</h3>
-    <EventContainer eventId={props.url.query.id} />
   </div>
 )
