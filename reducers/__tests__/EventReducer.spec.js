884ea42c470bbee39e783c3ea816793920233208
ytorii <metalwarrior_type_jp@hotmail.com>
2017-12-05 11:57:49 +0900

    Modify Event Reducer to use normalizr
    Modify Event Reducer spec
    
    Modify Participant Reducer
    
    Modify Participant Reducer spec
    
    Add participant reducer to combined reducer

--- reducers/__tests__/EventReducer.spec.js
+++ reducers/__tests__/EventReducer.spec.js
@@ -1,28 +1,32 @@
 import { createAction } from 'redux-actions'
-import { Set } from 'immutable'
+import { List } from 'immutable'
 
 import EventReducer, { eventInitialState } from '../event'
 import Actions from '../../constants/Actions'
 import ApiResponseError from '../../api/ApiResponseError'
-import ConvertCase from '../../utils/ConvertCase'
-
-import EventModel from '../../models/Event'
-import ParticipantModel from '../../models/Participant'
-
 import EventParams from '../../factories/Event'
 import ParticipantParams from '../../factories/Participant'
 
+const initialState = eventInitialState
+const testEventParams = EventParams.event1
+const testParticipantParams = ParticipantParams.participant1
+
+const testEventResult = initialState.merge({
+  entityId: testEventParams.id,
+  entities: { [testEventParams.id]: testEventParams },
+  errors: [],
+})
 
 const error = {
   response: { data: { name: ['を入力して下さい', 'は１０文字以下です'] } },
 }
 
-const errorMessages = ['nameを入力して下さい', 'nameは１０文字以下です']
+const errorMessages = new List(['nameを入力して下さい', 'nameは１０文字以下です'])
 
 describe('Event Reducer', () => {
   describe('when initial state', () => {
     it('should return the initial state', () => {
-      expect(EventReducer(undefined, {})).toEqual(eventInitialState)
+      expect(EventReducer(undefined, {})).toEqual(initialState)
     })
   })
 
@@ -31,15 +35,15 @@ describe('Event Reducer', () => {
 
     describe('with success event create', () => {
       it('should return created event', () => {
-        const eventState = EventReducer(null, createEvent(EventParams.event1))
-        expect(eventState).toEqual(new EventModel(EventParams.event1))
+        const subject = EventReducer(initialState, createEvent(testEventParams))
+        expect(subject).toEqual(testEventResult)
       })
     })
 
     describe('with failure event create', () => {
       it('should return error message', () => {
-        const eventState = EventReducer(null, createEvent(new ApiResponseError(error)))
-        expect(eventState.errors).toEqual(errorMessages)
+        const subject = EventReducer(initialState, createEvent(new ApiResponseError(error)))
+        expect(subject.get('errors')).toEqual(errorMessages)
       })
     })
   })
@@ -49,45 +53,40 @@ describe('Event Reducer', () => {
 
     describe('with success event fetch', () => {
       it('should return fetched event', () => {
-        const eventState = EventReducer(null, fetchEvent(EventParams.event1))
-        expect(eventState).toEqual(new EventModel(EventParams.event1))
+        const subject = EventReducer(initialState, fetchEvent(testEventParams))
+        expect(subject).toEqual(testEventResult)
       })
     })
 
     describe('with failure event fetch', () => {
       it('should return error message', () => {
-        const eventState = EventReducer(null, fetchEvent(new ApiResponseError(error)))
-        expect(eventState.errors).toEqual(errorMessages)
+        const subject = EventReducer(initialState, fetchEvent(new ApiResponseError(error)))
+        expect(subject.get('errors')).toEqual(errorMessages)
       })
     })
   })
 
-  describe('when JOIN_EVENT action', () => {
+  describe('when REGISTER_FOR_EVENT action', () => {
     const registerForEvent = createAction(Actions.Event.registerForEvent)
 
-    const participant = ParticipantParams.participant1
-    const receivedData = ConvertCase.snakeKeysOf(participant)
+    const prevState = testEventResult
+    const eventId = prevState.get('entityId').toString()
+    const nextState = prevState.updateIn(
+      ['entities', eventId, 'participants'],
+      participants => participants.push(testParticipantParams.id),
+    )
 
-    const prevState = new EventModel({
-      ...EventParams.event1,
-      errors: ['Error'],
-    })
-    const nextState = new EventModel({
-      ...EventParams.event1,
-      participants: Set.of(new ParticipantModel(participant)),
-    })
-
-    describe('with success event join', () => {
-      it('should return joined event', () => {
-        const eventState = EventReducer(prevState, registerForEvent(receivedData))
-        expect(eventState).toEqual(nextState)
+    describe('with success event register', () => {
+      it('should add registered participant', () => {
+        const subject = EventReducer(prevState, registerForEvent(testParticipantParams))
+        expect(subject).toEqual(nextState)
       })
     })
 
-    describe('with failure event join', () => {
-      it('should return error message', () => {
-        const eventState = EventReducer(prevState, registerForEvent(new ApiResponseError(error)))
-        expect(eventState).toEqual(prevState.set('errors', errorMessages))
+    describe('with failure event registerFor', () => {
+      it('should keep previous state', () => {
+        const subject = EventReducer(prevState, registerForEvent(new ApiResponseError(error)))
+        expect(subject).toEqual(prevState)
       })
     })
   })
---
+++ reducers/participant.js
@@ -0,0 +1,48 @@
+// @flow
+import { handleActions } from 'redux-actions'
+import { normalize } from 'normalizr'
+import { Map, List } from 'immutable'
+import _ from 'lodash'
+
+import Actions from '../constants/Actions'
+import EventSchema from '../schemas/event'
+import ParticipantSchema from '../schemas/participant'
+import ConvertCase from '../utils/ConvertCase'
+
+export const participantInitialState = new Map({
+  entities: new Map(),
+  errors: new List(),
+})
+
+const toCamelCase = payload => (
+  _.mapValues(payload, val => ConvertCase.camelKeysOf(val))
+)
+
+const participantReducerMap = {
+  [Actions.Event.fetchEvent]: {
+    next: (state, action) => {
+      const normalizedPayload = normalize(action.payload, EventSchema)
+      const participant = toCamelCase(normalizedPayload.entities.participant)
+
+      return state.merge({
+        entities: participant,
+        errors: [],
+      })
+    },
+  },
+
+  [Actions.Event.registerForEvent]: {
+    next: (state, action) => {
+      const normalizedPayload = normalize(action.payload, ParticipantSchema)
+      const participant = toCamelCase(normalizedPayload.entities.participant)
+
+      return state.mergeDeep({
+        entities: participant,
+        errors: [],
+      })
+    },
+    throw: (state, action) => state.merge({ errors: action.payload.errors }),
+  },
+}
+
+export default handleActions(participantReducerMap, participantInitialState)
