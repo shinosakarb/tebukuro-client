d24799cdb25dd3d2b033473199147b400cd43a97
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-12-10 02:21:39 +0900

    Add flow type

--- middlewares/normalize.js
+++ middlewares/normalize.js
@@ -1,11 +1,13 @@
+// @flow
 import { normalize } from 'normalizr'
+import type { ReduxAction, ActionPayload } from '../types/redux'
 
-const isPromise = payload => payload instanceof Promise
-const canNormalize = action =>
+const isPromise = (payload: ActionPayload) => payload instanceof Promise
+const canNormalize = (action: ReduxAction) =>
   !action.error && action.meta && action.meta.normalizr &&
   !isPromise(action.payload)
 
-const normalizeMiddleware = () => next => (action) => {
+const normalizeMiddleware = () => (next: Function) => (action: ReduxAction) => {
   if (canNormalize(action)) {
     const payload = normalize(action.payload, action.meta.normalizr.schema)
     return next({ ...action, payload })
