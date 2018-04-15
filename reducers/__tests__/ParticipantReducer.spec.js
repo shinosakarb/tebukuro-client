8c784b5577031dd87ed759626b251b535ec6f93c
ytorii <metalwarrior_type_jp@hotmail.com>
2018-04-15 10:53:24 +0900

    Simplify reducer specs

--- reducers/__tests__/ParticipantReducer.spec.js
+++ reducers/__tests__/ParticipantReducer.spec.js
@@ -1,38 +1,28 @@
 import { createAction } from 'redux-actions'
 import { Map, List } from 'immutable'
-import { normalize } from 'normalizr'
 
 import ParticipantReducer, { participantInitialState } from '../participant'
 import Actions from '../../constants/Actions'
 import Messages from '../../constants/Messages'
 import ApiResponseError from '../../api/ApiResponseError'
-import ConvertCase from '../../utils/ConvertCase'
 
-import EventParams from '../../factories/Event'
 import ParticipantParams from '../../factories/Participant'
 
-import EventSchema from '../../schemas/event'
-import ParticipantSchema from '../../schemas/participant'
-
 const { participant1, participant2 } = ParticipantParams
-const admittedParticipant = { ...participant1, onWaitingList: false }
-const waitlistedParticipant = { ...participant2, onWaitingList: true }
-const admittedParticipantEntity = { [admittedParticipant.id]: new Map(admittedParticipant) }
-const waitlistedParticipantEntity = { [waitlistedParticipant.id]: new Map(waitlistedParticipant) }
 
-const initialState = participantInitialState
-const participantMergedState = initialState.merge({ entities: admittedParticipantEntity })
+const participantPayload = participant => ({
+  result: participant.id,
+  entities: { participant: { [participant.id]: participant } },
+  errors: [],
+})
 
-const { admittedRegestration, waitlistedRegestration } = Messages.Participants
-const error = {
-  response: { data: { name: ['を入力して下さい', 'は１０文字以下です'] } },
-}
+const error = { response: { data: { name: ['を入力して下さい', 'は１０文字以下です'] } } }
 const errorMessages = new List(['nameを入力して下さい', 'nameは１０文字以下です'])
 
 describe('Participant Reducer', () => {
   describe('when initial state', () => {
     it('should return the initial state', () => {
-      expect(ParticipantReducer(undefined, {})).toEqual(initialState)
+      expect(ParticipantReducer(undefined, { type: '@@INIT' })).toEqual(participantInitialState)
     })
   })
 
@@ -41,22 +31,25 @@ describe('Participant Reducer', () => {
 
     describe('with success event fetch', () => {
       it('should return participants in fetched event', () => {
-        const eventParams = {
-          ...EventParams.event1,
-          participants: [ConvertCase.snakeKeysOf(admittedParticipant)],
-        }
-        const normalizedEvent = normalize(eventParams, EventSchema)
-        const subject = ParticipantReducer(initialState, fetchEvent(normalizedEvent))
+        const expectedState = new Map({
+          entities: new Map({
+            [participant1.id]: new Map({ ...participant1 }),
+          }),
+          errors: new List(),
+          message: null,
+        })
+        const payload = participantPayload(participant1)
+        const subject = ParticipantReducer(participantInitialState, fetchEvent(payload))
 
-        expect(subject).toEqual(participantMergedState)
+        expect(subject).toEqual(expectedState)
       })
     })
 
     describe('with failure event fetch', () => {
       it('should keep state', () => {
         const subject =
-          ParticipantReducer(initialState, fetchEvent(new ApiResponseError(error)))
-        expect(subject).toEqual(initialState)
+          ParticipantReducer(participantInitialState, fetchEvent(new ApiResponseError(error)))
+        expect(subject).toEqual(participantInitialState)
       })
     })
   })
@@ -64,31 +57,39 @@ describe('Participant Reducer', () => {
   describe('when REGISTER_FOR_EVENT action', () => {
     const registerForEvent = createAction(Actions.Event.registerForEvent)
 
-    const reducedParticipant = (participant) => {
-      const normalizedPayload = normalize(ConvertCase.snakeKeysOf(participant), ParticipantSchema)
-      return ParticipantReducer(initialState, registerForEvent(normalizedPayload))
-    }
 
     describe('with success registeration for event', () => {
-      describe('with onWaitingList true', () => {
+      describe('with onWaitingList false', () => {
         it('should add regitered participant with the admitted message.', () => {
-          const subject = reducedParticipant(admittedParticipant)
-          const nextState = initialState.mergeDeep({
-            entities: admittedParticipantEntity,
-            message: admittedRegestration,
+          const payload = participantPayload({ ...participant1, onWaitingList: false })
+          const subject = ParticipantReducer(participantInitialState, registerForEvent(payload))
+
+          const expectedState = new Map({
+            entities: new Map({
+              [participant1.id]: new Map({ ...participant1, onWaitingList: false }),
+            }),
+            errors: new List(),
+            message: Messages.Participants.admittedRegestration,
           })
-          expect(subject).toEqual(nextState)
+
+          expect(subject).toEqual(expectedState)
         })
       })
 
-      describe('with onWaitingList false.', () => {
+      describe('with onWaitingList true.', () => {
         it('should add regitered participant with the waitlisted message.', () => {
-          const subject = reducedParticipant(waitlistedParticipant)
-          const nextState = initialState.mergeDeep({
-            entities: waitlistedParticipantEntity,
-            message: waitlistedRegestration,
+          const payload = participantPayload({ ...participant1, onWaitingList: true })
+          const subject = ParticipantReducer(participantInitialState, registerForEvent(payload))
+
+          const expectedState = new Map({
+            entities: new Map({
+              [participant1.id]: new Map({ ...participant1, onWaitingList: true }),
+            }),
+            errors: new List(),
+            message: Messages.Participants.waitlistedRegestration,
           })
-          expect(subject).toEqual(nextState)
+
+          expect(subject).toEqual(expectedState)
         })
       })
     })
@@ -96,7 +97,7 @@ describe('Participant Reducer', () => {
     describe('with failure event register', () => {
       it('should return error message', () => {
         const subject =
-          ParticipantReducer(initialState, registerForEvent(new ApiResponseError(error)))
+          ParticipantReducer(participantInitialState, registerForEvent(new ApiResponseError(error)))
         expect(subject.get('errors')).toEqual(errorMessages)
       })
     })
@@ -107,21 +108,29 @@ describe('Participant Reducer', () => {
 
     describe('with success cancel registration', () => {
       it('should return participants in canceled event', () => {
-        const eventParams = {
-          ...EventParams.event1,
-          participants: [ConvertCase.snakeKeysOf(admittedParticipant)],
-        }
-        const normalizedEvent = normalize(eventParams, EventSchema)
-        const subject = ParticipantReducer(initialState, cancelRegistration(normalizedEvent))
+        const prevState = new Map({
+          entities: new Map({
+            [participant1.id]: new Map({ ...participant1 }),
+            [participant2.id]: new Map({ ...participant2 }),
+          }),
+          errors: new List(),
+          message: null,
+        })
+        const payload = participantPayload(participant1)
+        const subject = ParticipantReducer(prevState, cancelRegistration(payload))
 
-        expect(subject).toEqual(participantMergedState)
+        const expectedState = prevState.deleteIn(['entities', `${participant2.id}`])
+
+        expect(subject).toEqual(expectedState)
       })
     })
 
     describe('with failure cancel registration', () => {
       it('should return error message', () => {
-        const subject =
-          ParticipantReducer(initialState, cancelRegistration(new ApiResponseError(error)))
+        const subject = ParticipantReducer(
+          participantInitialState,
+          cancelRegistration(new ApiResponseError(error)),
+        )
         expect(subject.get('errors')).toEqual(errorMessages)
       })
     })
