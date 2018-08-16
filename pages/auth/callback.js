0d08785346d73c76c54765dcef9c21c8b3a76499
ytorii <metalwarrior_type_jp@hotmail.com>
2018-08-16 11:02:01 +0900

    Get auth info from window object on componentDidMount

--- pages/auth/callback.js
+++ pages/auth/callback.js
@@ -4,11 +4,24 @@ import Router from 'next/router'
 import { setAuthInfo, getAuthParams } from '../../utils/auth'
 import type { AuthInfo, AuthQueryParams } from '../../utils/auth'
 
-type Props = {
-  authInfo: AuthInfo,
+type Props = {}
+
+const getAuthInfoFrom = (queryStrings: String): AuthInfo => {
+  // Removing '?' in the head of string.
+  const query = queryStrings.slice(1).split('&').reduce((info, queryString) => {
+    const keyValue = queryString.split('=')
+    return Object.assign(info, { [keyValue[0]]: keyValue[1] })
+  }, {})
+
+  return {
+    accessToken: query.auth_token,
+    clientId: query.client_id,
+    uid: query.uid,
+  }
 }
 
 export default class AuthCallback extends Component<Props> {
+  // getInitialProps doesn't work when exported as static html.
   static async getInitialProps(ctx: { query: AuthQueryParams }) {
     return ctx.query ? {
       authInfo: getAuthParams(ctx.query),
@@ -16,7 +29,8 @@ export default class AuthCallback extends Component<Props> {
   }
 
   componentDidMount() {
-    setAuthInfo(this.props.authInfo)
+    const authInfo = getAuthInfoFrom(window.location.search)
+    setAuthInfo(authInfo)
     Router.replace('/')
   }
 
