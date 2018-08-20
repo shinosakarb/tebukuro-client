7b1cfa855dec8411c39a2b2e61a1780a483fde57
ogom <ogom@outlook.com>
2018-08-21 00:08:16 +0900

    Add material components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 // @flow
-import React from 'react'
-import Link from 'next/link'
-import UserInfo from '../components/UserInfo'
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Button from '@material/react-button';
+import UserInfo from '../components/UserInfo';
+import '../styles/style.css';
 
 export default () => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="/_next/static/style.css" />
+    </Head>
     <UserInfo />
     <div>Welcome to Tebukuro</div>
-    <Link href="event/new">
-      <a>Create a new event</a>
-    </Link>
+    <Button>
+      <Link href="event/new">
+        <a>Create a new event</a>
+      </Link>
+    </Button>
   </div>
-)
+);
